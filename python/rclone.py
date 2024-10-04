35b531f25674fb09339f532f617bac549f7dbd4c
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-10-04 17:06:27 +0200

    Create rclone.py

---
+++ python/rclone.py
@@ -0,0 +1,33 @@
+# Imports
+import subprocess
+
+
+# Function to back up data to OneDrive
+def main():
+    # Opening message
+    print('Python script to back up data to OneDrive via rclone.\n')
+
+    # Execute sync
+    command = ['./rclone', 'sync', 'data:', 'OneDrive:',    # Command
+                '--config', './rclone.conf',                # Flags
+                '--create-empty-src-dirs',
+                '--no-update-dir-modtime',
+                '--modify-window', '1s',
+                '--filter-from', './filters.md',
+                '--log-file', './rclone.log',
+                '--verbose']
+
+    print('Sync started...')
+    subprocess.run(command)
+    print('Sync finished!\n')
+
+    # Reminder to check logs
+    print('Check the logs to see if there were any errors.\n')
+
+    # Don't close window immediately when script finishes
+    input('Press enter to exit!')
+
+
+# Execute main when script is called directly
+if (__name__ == '__main__'):
+    main()
