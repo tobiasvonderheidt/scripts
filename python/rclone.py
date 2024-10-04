4e280997a885ef68abe962f845a7e7f201a7f12a
Tobias Vonderheidt <tobias@vonderheidt.org>
2024-10-04 18:17:13 +0200

    rclone.py: Select command based on user input

--- python/rclone.py
+++ python/rclone.py
@@ -7,30 +7,55 @@ def main():
     # Opening message
     print('Python script to back up data to OneDrive via rclone.\n')
 
-    # Execute sync
-    command = ['./rclone', 'sync', 'data:', 'OneDrive:',    # Command
-                '--config', './rclone.conf',                # Flags
-                '--create-empty-src-dirs',
-                '--no-update-dir-modtime',
-                '--modify-window', '1s',
-                '--filter-from', './filters.md',
-                '--log-file', './rclone.log',
-                '--verbose']
+    # Configure rclone commands
+    print('Choose commands to execute (y/n):')
 
-    print('Sync started...')
-    subprocess.run(command)
-    print('Sync finished!\n')
+    # sync
+    while (True):
+        sync = input('- Sync? ').strip().lower()
+
+        if (sync == 'y' or sync == 'n'):
+            break
+        else:
+            print('Please answer with (y/n).')
+
+    # cryptcheck
+    while (True):
+        cryptcheck = input('- Cryptcheck? ').strip().lower()
+
+        if (cryptcheck == 'y' or cryptcheck == 'n'):
+            break
+        else:
+            print('Please answer with (y/n).')
+
+    print('')   # Empty line to format output
+
+    # Execute sync
+    if (sync == 'y'):
+        command = ['./rclone', 'sync', 'data:', 'OneDrive:',    # Command
+                   '--config', './rclone.conf',                 # Flags
+                   '--create-empty-src-dirs',
+                   '--no-update-dir-modtime',
+                   '--modify-window', '1s',
+                   '--filter-from', './filters.md',
+                   '--log-file', './rclone.log',
+                   '--verbose']
+
+        print('Sync started...')
+        subprocess.run(command)
+        print('Sync finished!\n')
 
     # Execute cryptcheck
-    command = ['./rclone', 'cryptcheck', 'data:', 'OneDrive:',
-                '--config', './rclone.conf',
-                '--filter-from', './filters.md',
-                '--log-file', './rclone.log',
-                '--verbose']
+    if (cryptcheck == 'y'):
+        command = ['./rclone', 'cryptcheck', 'data:', 'OneDrive:',
+                   '--config', './rclone.conf',
+                   '--filter-from', './filters.md',
+                   '--log-file', './rclone.log',
+                   '--verbose']
 
-    print('Cryptcheck started...')
-    subprocess.run(command)
-    print('Cryptcheck finished!\n')
+        print('Cryptcheck started...')
+        subprocess.run(command)
+        print('Cryptcheck finished!\n')
 
     # Reminder to check logs
     print('Check the logs to see if there were any errors.\n')
